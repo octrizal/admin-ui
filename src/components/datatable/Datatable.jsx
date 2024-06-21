79be9a07c9df68a7c498d0499552943af77b1546
octrizal <66666158+octrizal@users.noreply.github.com>
2024-06-21 08:31:27 +0700

    ubah teks di app

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,6 +1,9 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { Link,useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {collection, onSnapshot, deleteDoc, doc} from "firebase/firestore";
+import { db } from "../../firebase";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
@@ -34,21 +37,60 @@ const rows = [
   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
 ];
 
-const Datatable = () => {
+const Datatable = ({columns}) => {
   const Location = useLocation();
   const type = location.pathname.split('/')[1];
 
+  const [data, setData] = useState([]);
+
+useEffect(() => {
+  const unsub = onSnapshot(
+    collection(db, type),
+    (snapShot) => {
+      let list = [];
+      snapShot.docs.forEach((doc) => {
+        list.push({ id: doc.id, ...doc.data() });
+      });
+      setData(list);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+
+  return () => {
+    unsub();
+  };
+}, [type]); 
+
+const handleDelete = async (id) => {
+  try {
+    await deleteDoc(doc(db, type, id));
+    setData(data.filter((item) => item.id !== id));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const actionColumn = [
-  { 
+  {
     field: "action",
     headerName: "Action",
     width: 200,
-    renderCell: () => {
+    renderCell: (params) => {
       return (
         <div className="cellAction">
-          <Link to={"/" + type + "/test"} style={{ textDecoration: "none" }}>
+          <Link to={"/" + type + "/" + params.row.id} style={{ textDecoration: "none" }}>
             <span className="viewButton">View</span>
           </Link>
+          <span>
+	          <span
+	            className="deleteButton"
+	            onClick={() => handleDelete(params.row.id)}
+	          >
+	            Delete
+	          </span>
+	        </span>
         </div>
       );
     },
@@ -64,7 +106,7 @@ const actionColumn = [
         </Link>
       </div>
       <DataGrid className="datagrid" 
-        rows={rows}
+        rows={data}
         columns={columns.concat(actionColumn)}
         initialState={{
           pagination: {
